e5882211d290d5ffaf1a1a27b512b34a49173e37
Marvindlls <marvindillenseger@gmail.com>
2025-01-20 12:25:54 +0100

    Add CategoryController and category index template; remove unused CSS styles and add viewport meta tag

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class CategoryController extends AbstractController
+{
+    //#[IsGranted('ROLE_USER')]
+    #[Route('/category', name: 'app_category', methods: ['GET'])]
+    public function index(CategoryRepository $cr): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => '$cr->findAll()', //Récupérer toutes les catégories
+        ]);
+    }
+}
